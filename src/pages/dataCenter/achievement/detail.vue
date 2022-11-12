17478523f58063e32f1065dbeb4433aab78e3869
NJR10byh <48895838+applenjr@users.noreply.github.com>
2022-11-12 18:55:58 +0800

    导入组件

---
+++ src/pages/dataCenter/achievement/detail.vue
@@ -0,0 +1,17 @@
+<!--
+  * @author baoyuhao
+  * @date 2022/11/12 17:13:28
+  * @description
+  * @version 0.1.0
+-->
+<template>
+
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/pages/dataCenter/achievement/index.vue
@@ -0,0 +1,185 @@
+<template>
+  <div>
+    <t-card class="list-card-container">
+      <t-row justify="space-between">
+        <div class="left-operation-container">
+          <p v-if="!!selectedRowKeys.length" class="selected-count">已选{{ selectedRowKeys.length }}项</p>
+        </div>
+        <div class="search-input">
+          <t-input class="searchInputStyle" v-model="searchValue.achievementName" placeholder="成果名称"
+                   clearable></t-input>
+          <t-input class="searchInputStyle" v-model="searchValue.achievementContractPerson" placeholder="成果联系人"
+                   clearable></t-input>
+          <t-input class="searchInputStyle" v-model="searchValue.jobNumber" placeholder="工号"
+                   clearable></t-input>
+          <t-button class="searchBtnStyle" @click="searchForm">
+            <t-icon name="search"></t-icon>
+            查询
+          </t-button>
+        </div>
+      </t-row>
+      <t-table
+        :data="formListTableData"
+        :columns="FORM_TABLE_COLUMNS"
+        row-key="id"
+        vertical-align="center"
+        hover
+        :pagination="pagination"
+        :selected-row-keys="selectedRowKeys"
+        :loading="dataLoading"
+        :header-affixed-top="{ offsetTop, container: getContainer }"
+        :horizontal-scroll-affixed-bottom="{offsetBottom:'64', container: getContainer }"
+        :pagination-affixed-bottom="{offsetBottom:'0', container: getContainer }"
+        @page-change="rehandlePageChange"
+        @select-change="rehandleSelectChange"
+        style="margin-top: 20px"
+      >
+        <template #settings="slotProps">
+          <t-button theme="primary" @click="getFormInfo(slotProps.row)">查看</t-button>
+          <t-button theme="warning" @click="editForm(slotProps.row)">修改</t-button>
+          <t-popconfirm content="确认删除吗?" theme="danger" @confirm="deleteForm(slotProps.row)">
+            <t-button theme="danger">删除</t-button>
+          </t-popconfirm>
+        </template>
+      </t-table>
+    </t-card>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "ListBase"
+};
+</script>
+
+<script setup lang="ts">
+import { ref, onMounted, computed } from "vue";
+import { useRouter } from "vue-router";
+import { MessagePlugin } from "tdesign-vue-next";
+import { getFormList } from "@/api/list";
+import { useSettingStore } from "@/store";
+import { prefix } from "@/config/global";
+
+import { FORM_TABLE_COLUMNS } from "./constants";
+
+const store = useSettingStore();
+const router = useRouter();
+
+/**
+ * data
+ */
+/**
+ * 表格相关
+ */
+// 表格加载
+const dataLoading = ref(false);
+// 表格数据
+const formListTableData = ref([]);
+// 表格分页
+const pagination = ref({
+  total: 100,
+  pageSize: 20,
+  current: 1
+});
+// 查询表格
+const searchValue = ref({
+  page: pagination.value.current,
+  size: pagination.value.pageSize,
+  achievementName: "",// 成果名称
+  achievementContractPerson: "",// 成果联系人
+  jobNumber: "" // 工号
+});
+// 已选择的row
+const selectedRowKeys = ref([]);
+// 根据是否使用多Tab页判断offsetTop
+const offsetTop = computed(() => {
+  return store.isUseTabsRouter ? 48 : 0;
+});
+
+
+/**
+ * methods
+ */
+/* 生命周期 */
+// 组件挂载完成后执行
+onMounted(() => {
+  // 获取表格数据
+  fetchData();
+});
+const getContainer = () => {
+  return document.querySelector(`.${prefix}-layout`);
+};
+// 获取表格数据
+const fetchData = async () => {
+  dataLoading.value = true;
+  try {
+    const { formList } = await getFormList();
+    console.log(formList);
+    formListTableData.value = formList;
+    pagination.value = {
+      ...pagination.value,
+      total: formList.length
+    };
+  } catch (e) {
+    console.log(e);
+  } finally {
+    dataLoading.value = false;
+  }
+};
+
+// 表格选择钩子
+const rehandleSelectChange = (val: number[]) => {
+  selectedRowKeys.value = val;
+};
+// 分页钩子
+const rehandlePageChange = (curr, pageInfo) => {
+  console.log("分页变化", curr, pageInfo);
+};
+// 查看详情
+const getFormInfo = (row) => {
+  console.log(row.id);
+};
+// 修改
+const editForm = (row) => {
+  console.log(row.id);
+};
+// 删除
+const deleteForm = (row) => {
+  console.log(row.id);
+};
+</script>
+
+<style lang="less" scoped>
+.payment-col {
+  display: flex;
+
+  .trend-container {
+    display: flex;
+    align-items: center;
+    margin-left: 8px;
+  }
+}
+
+.left-operation-container {
+  padding-top: 6px;
+
+  .selected-count {
+    display: inline-block;
+    margin-left: 8px;
+    color: var(--td-text-color-secondary);
+  }
+}
+
+.search-input {
+  //border: 1px solid red;
+  display: flex;
+
+  .searchInputStyle, .searchBtnStyle {
+    margin-left: 8px;
+  }
+
+  .searchBtnStyle {
+    width: 180px;
+  }
+}
+</style>
