7a2a1c80d88d896ee1bb25d6185a09e4c4f50d54
baoyuhao <baoyuhao@migu.cn>
2023-02-20 00:11:46 +0800

    上线

--- vite.config.ts
+++ vite.config.ts
@@ -55,15 +55,15 @@ export default ({ mode }: ConfigEnv): UserConfig => {
         /**
          * 测试环境
          */
-        "/collection_dev": {
-          target: "http://47.115.206.210:8080",
-          changeOrigin: true,
-          rewrite: (path) => path.replace(/^\/collection_dev/, "")
-        }
+        // "/collection_dev": {
+        //   target: "http://47.115.206.210:8080",
+        //   changeOrigin: true,
+        //   rewrite: (path) => path.replace(/^\/collection_dev/, "")
+        // }
         /**
          * 现网环境
          */
-        // "/collection_dev": "http://cgzh.njupt.edu.cn:8080" // 开发环境
+        "/collection_dev": "http://cgzh.njupt.edu.cn:8080" // 开发环境
       }
     }
   };
